2d722216d07d26054f315c8c1a5f6a3b69e22628
angy <angy.paolatabares@gmail.com>
2023-01-10 11:19:07 -0500

    sign in complete

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -34,7 +34,7 @@ export default function CartScreen() {
   };
 
   const checkoutHandler = () => {
-    navigate("/sigin?redirect=/shipping");
+    navigate("/signin?redirect=/shipping");
   };
   return (
     <div>
