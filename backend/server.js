9aa27943c285ff890685f89e197dd625b1c905b6
angy <angy.paolatabares@gmail.com>
2025-09-18 19:28:47 -0500

    feat: add new route

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/api/health", (req, res) => {
+  res.json({ status: "OK", message: "Estoy despierto" });
+});
+
 app.get("/api/keys/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
